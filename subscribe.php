0f9da30e0a44534c6506c79cda47fcfd881bc261
kamlingfong <64257320+kamlingfong@users.noreply.github.com>
2020-06-23 23:38:32 +0800

    Skills

---
+++ subscribe.php
@@ -0,0 +1,66 @@
+<?php
+if (isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "lisa@prehireforms.com";
+    $email_subject = "New Subscription";
+
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['email'])
+
+     {
+        problem('We are sorry, but there appears to be a problem with the email you submitted.');
+    }
+        !isset($_POST['email'])
+
+    $email = $_POST['email']; // required
+
+    $error_message = "Error! Please write in to lisa@prehireforms.com to subscribe!";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "fname: " . clean_string($name) . "\n";
+    $email_message .= "email: " . clean_string($email) . "\n";
+    $email_message .= "message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
+}
+?>
